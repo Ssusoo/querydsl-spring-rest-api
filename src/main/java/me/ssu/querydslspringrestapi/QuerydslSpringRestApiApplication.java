b333b0d6344d4527d4e5088c248d137841181d25
김수환 <zzanggoon8@hiconsy.com>
2022-12-29 12:43:28 +0900

    first commit

---
+++ src/main/java/me/ssu/querydslspringrestapi/QuerydslSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.ssu.querydslspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ssu/querydslspringrestapi/QuerydslSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.querydslspringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
