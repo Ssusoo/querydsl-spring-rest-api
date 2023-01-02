32427a6796071a0224c6c22baad14d5547396e9a
김수환 <zzanggoon8@hiconsy.com>
2023-01-02 19:18:46 +0900

    KISA SEED 암호화 알고리즘 Add

--- src/main/java/me/ssu/querydslspringrestapi/config/properties/EnvProperties.java
+++ src/main/java/me/ssu/querydslspringrestapi/config/properties/EnvProperties.java
@@ -5,6 +5,9 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * KISA SEED 암호화 알고리즘
+ */
 @Component
 @ConfigurationProperties(prefix = "env")
 @Setter
