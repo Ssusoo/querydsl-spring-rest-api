35432d143b81c416a15fc7c0c01d8db789b901a7
김수환 <zzanggoon8@hiconsy.com>
2023-01-03 23:19:35 +0900

    Application Modified

--- src/main/java/me/ssu/querydslspringrestapi/Application.java
+++ src/main/java/me/ssu/querydslspringrestapi/Application.java
@@ -2,12 +2,40 @@ package me.ssu.querydslspringrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+@EnableAspectJAutoProxy
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	/**
+	 * HttpServletRequest 를 Thread safe 하게 사용할 수 있도록 설정 추가
+	 */
+	@Override
+	public void onStartup(ServletContext servletContext) {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+		dispatcherServlet.setThreadContextInheritable(true);
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcherServlet", dispatcherServlet);
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
 }
