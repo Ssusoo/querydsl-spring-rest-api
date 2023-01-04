7cdfb2dee99e7844f63194585a6cf14e1ffecc87
zzanggoon8 <zzanggoon8@gmail.com>
2023-01-04 23:29:13 +0900

    Member Entity Modified

--- src/main/java/me/ssu/querydslspringrestapi/domains/member/domain/MemberUpdateHistory.java
+++ src/main/java/me/ssu/querydslspringrestapi/domains/member/domain/MemberUpdateHistory.java
@@ -2,32 +2,30 @@ package me.ssu.querydslspringrestapi.domains.member.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Table(name = "ssu_mem_mst")
+@Table(name = "ssu_mem_upd_hist")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
-public class MemberMaster {
+public class MemberUpdateHistory {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "mem_serlno")
-	private Long memberSerialNo; // 회원_일련번호
+	@Column(name = "mem_upd_hist_serlno", nullable = false, updatable = false)
+	private Long memberUpdateHistorySerialNo; // 회원_수정이력_일련번호
 
-	@Column(name = "mem_email", length = 200, nullable = false, updatable = false)
-	private String memberEmail; // 회원_이메일
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "mem_serlno", nullable = false, updatable = false,
+			foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private MemberMaster memberMaster; // 회원_일련번호
 
-	@Column(name = "mem_nm", length = 100, nullable = false, updatable = false)
-	private String memberName; // 회원_이름
-
-	@Column(name = "mem_pwd", length = 150, nullable = false)
-	private String memberPassword; // 회원_비밀번호
+	@Column(name = "mem_email", length = 200, nullable = false)
+	private String memberEmail;
 
 	@Column(name = "birthymd")
 	private Date birthDate; // 생년월일
@@ -35,11 +33,17 @@ public class MemberMaster {
 	@Column(name = "mem_gndr_flag", length = 1)
 	private String memberGenderFlag; // 회원_성별_플래그
 
+	@Column(name = "mem_telno", length = 20)
+	private String memberTelephoneNumber; // 회원_전화번호
+
 	@Column(name = "mem_mobile_telno", length = 20)
 	private String memberMobileTelephoneNumber; // 회원_모바일_전화번호
 
-	@Column(name = "mem_ci", length = 88)
-	private String memberConnectingInformation; // 회원_CI(연계정보)
+	@Column(name = "legal_guardian_nm", length = 100)
+	private String legalGuardianName; // 법적_보호자_이름
+
+	@Column(name = "legal_guardian_mp_telno", length = 20)
+	private String legalGuardianMobilePhoneTelephoneNumber;  // 법적_보호자_휴대폰_전화번호
 
 	@Column(name = "bas_addr", length = 200)
 	private String basicAddress; // 기본_주소
@@ -50,14 +54,10 @@ public class MemberMaster {
 	@Column(name = "zipcode", length = 6)
 	private String zipcode; // 우편번호
 
-	@Column(name = "use_yn", length = 1, nullable = false)
-	private String useYn; // 사용 여부
+	@Column(name = "upd_mngr_serlno")
+	private Long updateManagerSerialNo; // 수정_관리자_일련번호
 
 	@CreatedDate
-	@Column(name = "reg_dtm", nullable = false, updatable = false)
-	private LocalDateTime registerAt; // 등록_일시
-
-	@LastModifiedDate
 	@Column(name = "upd_dtm", nullable = false)
 	private LocalDateTime updateAt; // 수정_일시
 }
