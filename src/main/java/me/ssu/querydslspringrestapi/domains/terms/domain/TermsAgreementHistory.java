d2ecbe445bb9e0f94305f1ca9271cf685019f6a6
김수환 <zzanggoon8@hiconsy.com>
2023-01-06 23:20:06 +0900

    TermsAgreementHistory Entity Add

---
+++ src/main/java/me/ssu/querydslspringrestapi/domains/terms/domain/TermsAgreementHistory.java
@@ -0,0 +1,41 @@
+package me.ssu.querydslspringrestapi.domains.terms.domain;
+
+import lombok.*;
+import me.ssu.querydslspringrestapi.domains.member.domain.MemberMaster;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ssu_terms_agrmt_hist")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class TermsAgreementHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "terms_agrmt_hist_serlno", nullable = false, updatable = false)
+	private Long termsAgreementHistorySerialNo; // 약관_동의_이력_일련번호
+
+	// 회원 Entity 생성 후 주석 해제
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "himm_mem_serlno", nullable = false, updatable = false,
+			foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private MemberMaster memberMaster; // 회원_일련번호
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumns(value = {
+			@JoinColumn(name = "terms_cd", nullable = false, updatable = false),
+			@JoinColumn(name = "terms_turn_ord_no", nullable = false, updatable = false)
+	}, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private Terms terms;
+
+	@Column(name = "terms_agrmt_yn")
+	private String termsAgreementYn; // 약관_동의_여부
+
+	@CreatedDate
+	@Column(name = "reg_dtm", nullable = false)
+	private LocalDateTime registerAt; // 등록_일시
+}
