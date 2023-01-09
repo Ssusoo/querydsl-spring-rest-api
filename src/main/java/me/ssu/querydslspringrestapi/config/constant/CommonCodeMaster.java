e80ffdca8125ed1bb1160abe3b1db0f0a120408e
김수환 <zzanggoon8@hiconsy.com>
2023-01-09 23:32:03 +0900

    CommonCodeMaster CommonCode Modified

--- src/main/java/me/ssu/querydslspringrestapi/config/constant/CommonCodeMaster.java
+++ src/main/java/me/ssu/querydslspringrestapi/config/constant/CommonCodeMaster.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum CommonCodeMaster {
-	MEET_QUESTION_STATUS("SA01", "sampleCode", ""),
 
+	TERMS_DIVISION("HI01", "termsDivisionCommonCode", ""), // 약관_구분_공통_코드
+	TERMS_KIND("HI02", "", "termsKindCommonCode"), // 약관_종류_공통_코드
+	TERMS_EXPOSURE("HI03", "termsExposureCommonCode", ""), // 약관_노출_공통_코드
 	;
 
 	private final String code; // 마스터 코드
