6366ee1526f2004f2ca3fc45a9b27cbb197e13ec
김수환 <zzanggoon8@hiconsy.com>
2023-01-10 00:02:20 +0900

    Import Opimized

--- src/main/java/me/ssu/querydslspringrestapi/domains/member/application/SignUpTermsService.java
+++ src/main/java/me/ssu/querydslspringrestapi/domains/member/application/SignUpTermsService.java
@@ -1,6 +1,5 @@
 package me.ssu.querydslspringrestapi.domains.member.application;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.ssu.querydslspringrestapi.config.error.DataNotFoundException;
@@ -14,8 +13,6 @@ import me.ssu.querydslspringrestapi.domains.terms.repository.TermsMemberAgreemen
 import me.ssu.querydslspringrestapi.domains.terms.repository.TermsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @Slf4j
