cb8a3027e2d9ee0722de6d2c91a80f066c737957
김수환 <zzanggoon8@hiconsy.com>
2023-01-15 12:24:58 +0900

    NotificationCertification Entity Modified

--- src/main/java/me/ssu/querydslspringrestapi/domains/common/domain/NotificationCertification.java
+++ src/main/java/me/ssu/querydslspringrestapi/domains/common/domain/NotificationCertification.java
@@ -21,6 +21,9 @@ public class NotificationCertification {
 	@Column(name = "noti_cert_serlno", nullable = false, updatable = false)
 	private Long notificationCertificationSerialNo; // 알림_인증_일련번호
 
+	@Column(name = "mem_serlno", nullable = false)
+	private Long memberSerialNo; // 회원_일련번호
+
 	@Column(name = "mem_nm", length = 100)
 	private String memberName; // 회원_이름
 
@@ -42,31 +45,31 @@ public class NotificationCertification {
 	@Column(name = "noti_cont")
 	private String notificationContents; // 알림_내용
 
-	@Column(name = "noti_send_tagt_div_comm_cd", nullable = false)
+	@Column(name = "noti_send_tagt_div_comm_cd", length = 8, nullable = false)
 	private String notificationSendTargetDivisionCommonCode; // 알림_발신_대상_구분_공통_코드
 
-	@Column(name = "noti_recp_tagt_div_comm_cd", nullable = false)
+	@Column(name = "noti_recp_tagt_div_comm_cd", length = 8, nullable = false)
 	private String notificationReceptionTargetDivisionCommonCode; // 알림_수신_대상_구분_공통_코드
 
-	@Column(name = "noti_sed_div_comm_cd")
+	@Column(name = "noti_sed_div_comm_cd", length = 8)
 	private String notificationSendDivisionCommonCode; // 알림_발송_구분_공통_코드
 
-	@Column(name = "noti_tmplt_kind_comm_cd", nullable = false)
+	@Column(name = "noti_tmplt_kind_comm_cd", length = 8, nullable = false)
 	private String notificationTemplateKindCommonCode; // 알림_템플릿_종류_공통_코드
 
-	@Column(name = "noti_lcasf_comm_cd", nullable = false)
+	@Column(name = "noti_lcasf_comm_cd", length = 8, nullable = false)
 	private String notificationLargeClassificationCommonCode; // 알림_대분류_공통_코드
 
-	@Column(name = "noti_smclasf_comm_cd", nullable = false)
+	@Column(name = "noti_smclasf_comm_cd", length = 8, nullable = false)
 	private String notificationSmallClassificationCommonCode; // 알림_소분류_공통_코드
 
-	@Column(name = "noti_kind_comm_cd", nullable = false)
+	@Column(name = "noti_kind_comm_cd", length = 8, nullable = false)
 	private String notificationKindCommonCode; // 알림_종류_공통_코드
 
-	@Column(name = "noti_stat_div_comm_cd", nullable = false)
+	@Column(name = "noti_stat_div_comm_cd", length = 8, nullable = false)
 	private String notificationStatusDivisionCommonCode; // 알림_상태_구분_공통_코드
 
-	@Column(name = "noti_resv_div_comm_cd", nullable = false)
+	@Column(name = "noti_resv_div_comm_cd", length = 8, nullable = false)
 	private String notificationReservationDivisionCommonCode; // 알림_예약_구분_공통_코드
 
 	@Column(name = "send_yn", length = 1, nullable = false)
