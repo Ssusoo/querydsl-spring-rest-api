770c8e0e958feffdf5336184e0b9853b21def62b
zzanggoon8 <zzanggoon8@gmail.com>
2023-01-16 23:18:18 +0900

    Member Master Dto Modified

--- src/main/java/me/ssu/querydslspringrestapi/domains/member/dto/MemberSignUp.java
+++ src/main/java/me/ssu/querydslspringrestapi/domains/member/dto/MemberSignUp.java
@@ -2,10 +2,9 @@ package me.ssu.querydslspringrestapi.domains.member.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import me.ssu.querydslspringrestapi.config.constant.CommonCodeMaster;
-import me.ssu.querydslspringrestapi.domains.common.application.CommonCodesService;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
@@ -13,41 +12,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberSignUp {
 
 	@Getter
 	public class Request {
-		@Schema(defaultValue = "TERMS00001", description = "약관_코드")
-		private String termsCode;
-
-		@Schema(defaultValue = "1", description = "약관_회차_번호")
-		private Integer termsTurnOrderNo;
-
-		@Schema(defaultValue = "Y", description = "동의_여부")
-		private String agreementYn;
-
-		@Schema(defaultValue = "Y", description = "약관_동의_여부")
-		private String termsAgreementYn;
-
-		@Schema(defaultValue = "HI080001", description = "약관_구분_공통_코드 - 필수, 선택")
-		private String termsDivisionCommonCode;
-
-		@Schema(defaultValue = "필수", description = "약관_구분_공통_코드_이름")
-		private String termsDivisionCommonCodeName;
-
-		@Schema(defaultValue = "HI100001", description = "약관_노출_공통_코드 - 회원가입 사용 약관, 그외 부가적인 약관")
-		private String termsExposureCommonCode;
-
-		@Schema(defaultValue = "회원가입 약관", description = "약관_노출_공통_코드_이름")
-		private String termsExposureCommonCodeName;
-
-		@Schema(defaultValue = "HI090001", description = "약관_종류_구분_코드(이용약관 / 개인정보 수집 및 이용 동의 / 개인정보 제3자 제공 동의 / 광고성 정보 수신 동의)")
-		private String termsKindCommonCode;
-
-		@Schema(defaultValue = "이용약관", description = "약관_종류_구분_코드_이름")
-		private String termsKindCommonCodeName;
+		private List<Terms> terms;
 
 		@Email
 		@NotBlank
@@ -95,49 +67,30 @@ public class MemberSignUp {
 
 		@Schema(defaultValue = "123040", description = "우편번호")
 		private String zipcode;
+	}
 
-		public Request(String termsCode, Integer termsTurnOrderNo, String agreementYn,
-		               String termsAgreementYn, String termsDivisionCommonCode,
-		               String termsExposureCommonCode, String termsKindCommonCode,
-		               String memberEmail, String memberName, String memberPassword,
-		               String checkMemberPassword, LocalDateTime birthDate, String memberGenderFlag,
-		               String memberMobileTelephoneNumber, String basicAddress,
-		               String detailAddress, String zipcode) {
-			this.termsCode = termsCode;
-			this.termsTurnOrderNo = termsTurnOrderNo;
-			this.agreementYn = agreementYn;
-			this.termsAgreementYn = termsAgreementYn;
-			this.termsDivisionCommonCode = termsDivisionCommonCode;
-			this.termsExposureCommonCode = termsExposureCommonCode;
-			this.termsKindCommonCode = termsKindCommonCode;
-			this.memberEmail = memberEmail;
-			this.memberName = memberName;
-			this.memberPassword = memberPassword;
-			this.checkMemberPassword = checkMemberPassword;
-			this.birthDate = birthDate;
-			this.memberGenderFlag = memberGenderFlag;
-			this.memberMobileTelephoneNumber = memberMobileTelephoneNumber;
-			this.basicAddress = basicAddress;
-			this.detailAddress = detailAddress;
-			this.zipcode = zipcode;
+	@Getter
+	@AllArgsConstructor(access = AccessLevel.PUBLIC)
+	public static class Terms {
+		@Schema(defaultValue = "TERMS00001", description = "약관_코드")
+		private String termsCode;
 
-			var commonCodes = CommonCodesService.getCommonCodes(
-					new CommonCodeMaster[]{
-							CommonCodeMaster.TERMS_DIVISION, // 약관_구분_공통_코드(필수/선택)
-							CommonCodeMaster.TERMS_EXPOSURE, // 약관_노출_공통_코드(회원가입)
-							CommonCodeMaster.TERMS_KIND, // 약관_종류_구분_코드_이름(이용약관)
-					});
+		@Schema(defaultValue = "1", description = "약관_회차_번호")
+		private Integer termsTurnOrderNo;
 
-			this.termsDivisionCommonCodeName = CommonCodesService
-					.getCodeName(commonCodes, CommonCodeMaster.TERMS_DIVISION, termsDivisionCommonCode);
+		@Schema(defaultValue = "Y", description = "동의_여부")
+		private String agreementYn;
 
-			this.termsExposureCommonCodeName = CommonCodesService
-					.getCodeName(commonCodes, CommonCodeMaster.TERMS_EXPOSURE, termsExposureCommonCode);
+		@Schema(defaultValue = "Y", description = "약관_동의_여부")
+		private String termsAgreementYn;
 
-			this.termsKindCommonCodeName = CommonCodesService
-					.getCodeName(commonCodes, CommonCodeMaster.TERMS_KIND, termsKindCommonCode);
+		@Schema(defaultValue = "HI080001", description = "약관_구분_공통_코드 - 필수, 선택")
+		private String termsDivisionCommonCode;
 
+		@Schema(defaultValue = "HI100001", description = "약관_노출_공통_코드 - 회원가입 사용 약관, 그외 부가적인 약관")
+		private String termsExposureCommonCode;
 
-		}
+		@Schema(defaultValue = "HI090001", description = "약관_종류_구분_코드(이용약관 / 개인정보 수집 및 이용 동의 / 개인정보 제3자 제공 동의 / 광고성 정보 수신 동의)")
+		private String termsKindCommonCode;
 	}
 }
